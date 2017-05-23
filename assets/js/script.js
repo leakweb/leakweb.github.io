6e8577ff480909060baf418649a6c3a290d512a3
_bogeyman_ <dewamah@gmail.com>
2017-05-23 20:46:31 +0800

    Update tampilan

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$("document").ready(function(){
+ new WOW().init();
+ $('.navbar-fixed').hide();
+ $(".button-collapse").sideNav({closeOnClick: true});
+ $('.parallax').parallax();
+ $('.section').scrollSpy();
+});
